34b5ededab1018105f80dce009bdb5f514b7cd4b
Dinopandab <coronalizbeth567@gmail.com>
2021-08-09 12:58:51 -0400

    lesson 3, lesson 2, lesson 5

--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,24 @@
 public class L4 {
+    public static void main(String[] args) {
+
+        int value = 5;
+        //REUSE YOU DO NOT NEED TO DECLARE
+
+        // Declare a variable - stating the datatype and identifier
+
+        double total;
+        String message = "Hi";
+        final double  GRAVITY = -9.8;
+
+        //assign a value
+        total = 6;
+
+        System.out.println(message + ", the value is " + total );
+
+        total = 10;
+        message = "Bye";
+        //GRAVITY = -10; -- dis is baaaaaaaaaaaaad
+
+        System.out.println(message  + ", the value is " + total );
+    }
 }
